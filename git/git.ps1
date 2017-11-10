2c22c1d94c47d245a151c5da4f09767f78b0c634
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2017-11-10 09:33:37 +0000

    more things

--- git/git.ps1
+++ git/git.ps1
@@ -1,10 +1,23 @@
 cd C:\g
-if(Test-Path("gitdemo")) { `
-    Remove-Item gitdemo -Recurse -Force `
+if(Test-Path("gitdemo")) {
+    Remove-Item gitdemo -Recurse -Force
 }
 mkdir gitdemo
 cd gitdemo
 git init
 New-Item "ReadMe.md" -ItemType File
-git branch -vvvvvv
-
+git status
+git add .
+git status
+git commit -m "adding readme"
+git log
+# git cat-file -p
+&start "C:\Program Files\Git\git-bash.exe"
+Set-Content -path ".\ReadMe.md" -Value "# Hello World"
+git status
+git add .
+git status
+git commit -m "Adding Hello World Text"
+git log
+git branch
+git checkout -b "newone"
