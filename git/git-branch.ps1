273536a401199684e9e4259af61411e25f6dd23a
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2019-02-11 14:54:27 +0000

    refining branches

--- git/git-branch.ps1
+++ git/git-branch.ps1
@@ -3,30 +3,26 @@ cd "C:\g"
 if(Test-Path("C:\g\gitdemo")) {
     cd "gitdemo"
 }
-if(!(Test-Path("C:\g\gitdemo"))) {
-    . "C:\g\Presentations\git\git-clone.ps1"
-}
-start powershell
+
 "Read to go!!!"
-git fetch
 git checkout master
-git remote -vvvvv
-git branch -vvvvvv
-git branch -r -vvvvv
-git branch newone
+git branch contributing
 git branch
-git branch -D newone
-git checkout -b newone
+git branch -D contributing
 git branch
+git checkout -b contributing
+git branch
+New-Item "contributing.md" -ItemType File
+git add .
+git commit -m "creating contibute file"
+Set-Content -path ".\contributing.md" -Value "# come and drink from the fountain"
+git add .
+git commit -m "creating content"
+git checkout -b license
+New-Item "LICENSE" -ItemType File
+git add .
+git commit -m "creating license file"
+Set-Content -path ".\LICENSE" -Value "Give us your money"
 git checkout master
-git branch
-git branch -D newone
-git push origin head:newone
-git branch -r
-git checkout newone
-git branch -vvvvv
-git checkout master
-git push origin :newone
-git branch -r
-git branch -D newone
-git branch
+git merge contibuting
+git merge license
